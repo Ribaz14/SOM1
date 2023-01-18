67aba0758856108a38cf541a1711584077577476
Ribaz14 <122978737+Ribaz14@users.noreply.github.com>
2023-01-18 11:28:45 +0000

    Atualização pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
 input.onSound(DetectedSound.Loud, function () {
-	
+    ligaluz = !(ligaluz)
+    if (true) {
+        basic.showString("AMD RADEOM GRAPHICS")
+    } else {
+        basic.clearScreen()
+    }
 })
+let ligaluz = false
 input.setSoundThreshold(SoundThreshold.Loud, 128)
